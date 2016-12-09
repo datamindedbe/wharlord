395783787ff131bd57713011dc394c3fee2d6c6a
Mathias Lavaert <mathias.lavaert@dataminded.be>
2016-12-09 14:58:22 +0100

    Formatted Runner.scala

--- src/main/scala/be/dataminded/wharlord/Runner.scala
+++ src/main/scala/be/dataminded/wharlord/Runner.scala
@@ -2,21 +2,20 @@ package be.dataminded.wharlord
 
 import be.dataminded.wharlord.reporters.Reporter
 
-
 /**
- * An object responsible for running checks and producing reports
- */
+  * An object responsible for running checks and producing reports
+  */
 object Runner {
 
   /**
-   * Run checks and then report to the reporters. Each check will be reported by every reporter.
-   *
-   * @param checks An iterable of check objects to be reported
-   * @param reporters An iterable of reporters
-   * @return Result for every check passed as an argument
-   */
+    * Run checks and then report to the reporters. Each check will be reported by every reporter.
+    *
+    * @param checks An iterable of check objects to be reported
+    * @param reporters An iterable of reporters
+    * @return Result for every check passed as an argument
+    */
   def run(checks: Iterable[Check], reporters: Iterable[Reporter]): Map[Check, CheckResult] = {
-    checks.map(check => {
+    checks.map { check =>
       val potentiallyPersistedDf = check.cacheMethod.map(check.dataFrame.persist).getOrElse(check.dataFrame)
       val constraintResults = check.constraints.map(c => (c, c.fun(potentiallyPersistedDf))).toMap
       val numRows = potentiallyPersistedDf.count
@@ -25,7 +24,6 @@ object Runner {
 
       reporters.foreach(_.report(checkResult))
       (check, checkResult)
-    }).toMap
+    }.toMap
   }
-
 }
